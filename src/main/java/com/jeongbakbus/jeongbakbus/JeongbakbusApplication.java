634a792eb8f99c9231980e0fe03a00d23c7f4767
Bak Hyegyeong <calla3494qhk@naver.com>
2024-07-27 17:21:46 +0900

    initial commit

---
+++ src/main/java/com/jeongbakbus/jeongbakbus/JeongbakbusApplication.java
@@ -0,0 +1,13 @@
+package com.jeongbakbus.jeongbakbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeongbakbusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JeongbakbusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jeongbakbus/jeongbakbus/JeongbakbusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jeongbakbus.jeongbakbus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeongbakbusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
